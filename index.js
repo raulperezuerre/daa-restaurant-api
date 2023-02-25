90385e2479b329b8241f215f923a5df9ae27888f
raulperezgzz <raul.perez.gzz@gmail.com>
2023-02-25 01:42:44 -0600

    added users api

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
+const users = [{ name: 'Hugo' }, { name: 'Paco' }, { name: 'Luis' }];
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/users', (req, res) => res.json(users));
 
 app.listen(3000, () => console.log(`Server listening at port ${port}`));
