cf89622f294101d13552a51d6975448e2c58cb4b
raulperezgzz <raul.perez.gzz@gmail.com>
2023-02-25 02:29:27 -0600

    added cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 const users = [{ name: 'Hugo' }, { name: 'Paco' }, { name: 'Luis' }];
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/users', (req, res) => res.json(users));
